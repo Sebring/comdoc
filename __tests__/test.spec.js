5a916efa61e11e8be58bb4b12369149e921347b1
Sebring <johan.sebring@gmail.com>
2021-02-14 22:44:35 +0100

    tests: handle arrays for common tests

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -1,4 +1,5 @@
 const Comdoc = require('../comdoc.js')
+const FPO = require('fpo')
 
 	let lines = null
 	let parts = null
@@ -34,16 +35,45 @@ describe('Comdoc', () => {
 	})
 
 	describe('test1.marko', () => {
-		it('should have 21 lines', () => {
-				expect(lines.length).toBe(21)
+		it('has 22 lines', () => {
+			expect(lines.length).toBe(22)
 		})
-
-		it('should contain 3 parts', () => {
+		
+		it('contains 3 parts', () => {
 			expect(parts.length).toBe(3)
 		})
+		it('has sections', () => {
+			//console.log(sections)
+			expect(sections.length).toBe(4)
+			expect(sections[1].comments[0].length).toBeGreaterThan(5)
+			expect(sections[2].comments[0]).toBeUndefined()
+			
+		})
+		describe('parts', () => {
+			testSectionsComments([[0,36], [1,15] , [2, 0]])
+		})
 
-		it('should have 3 sections', () => {
-			expect(sections.length).toBe(3)
+		describe('sections', () => {
+			let langTest = [
+				[0, 'javascript'],
+				[1, 'javascript'],
+				[2, 'xml'],
+				[3, 'less']
+			]
+			testSectionsLang(langTest)
 		})
 	})
+
+	function testSectionsLang(list) { 
+		test.each(list)('expect section %i to be lang %s', (index, lang) => {
+			expect(sections[index].lang).toBe(lang)
+		})
+	}
+
+	function testSectionsComments(list) {
+		test.each(list)('expect section %i to have comment of length %i', (index, commentLength) => {
+			commentLength && expect(sections[index].comments[0].length).toBe(commentLength)
+			commentLength || expect(sections[index].comments[0]).toBeUndefined()
+		})
+	}
 })
